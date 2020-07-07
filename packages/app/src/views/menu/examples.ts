e7e5ca5bb73afdba4c67ccd7ccd6ac40814166d7
Maxim Balaganskiy <m.balaganskiy@gmail.com>
2020-07-08 09:30:45 +1000

    feat(app): add basic text-field example

--- packages/app/src/views/menu/examples.ts
+++ packages/app/src/views/menu/examples.ts
@@ -1,14 +1,3 @@
 import { MdcMenu } from '@aurelia-mdc-web/menu';
 
-export class Examples {
-  button: HTMLButtonElement;
-
-  menu: MdcMenu;
-
-  attached() {
-    this.button.addEventListener('click', () => {
-      this.menu.open = !this.menu.open;
-    });
-  }
-
-}
+export class Examples { }
