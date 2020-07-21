7f9142f44fd92639cf573e9aa156a038ec60d45e
Maxim Balaganskiy <m.balaganskiy@gmail.com>
2020-07-21 22:13:16 +1000

    feat(lookup): add not found/searching/error templates

--- packages/lookup/src/mdc-lookup-configuration.ts
+++ packages/lookup/src/mdc-lookup-configuration.ts
@@ -1,3 +1,5 @@
 export class MdcDefaultLookupConfiguration {
   public debounce: number = 850;
+  public searchingMessage: string = 'Searching...';
+  public notFoundMessage: string = 'Not found';
 }
