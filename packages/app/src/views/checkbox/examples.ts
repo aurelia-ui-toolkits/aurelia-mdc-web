b63bccf3e3438333bf8fb50b524b8e9beaa8b457
Maxim Balaganskiy <m.balaganskiy@gmail.com>
2020-07-29 19:37:17 +1000

    feat(app): add more checkbox examples

--- packages/app/src/views/checkbox/examples.ts
+++ packages/app/src/views/checkbox/examples.ts
@@ -1,3 +1,34 @@
 export class Examples {
   checked: boolean = true;
+
+  indeterminate: boolean = true;
+  indeterminateToChecked: boolean = true;
+  alignEnd: boolean;
+  disabled: boolean;
+  disableRipple: boolean;
+  checked2: boolean = true;
+
+  setIndeterminate() {
+    this.indeterminate = true;
+  }
+
+  toggle() {
+    this.checked2 = !this.checked2;
+  }
+
+  toggleIndeterminateToChecked() {
+    this.indeterminateToChecked = !this.indeterminateToChecked;
+  }
+
+  toggleAlignEnd() {
+    this.alignEnd = !this.alignEnd;
+  }
+
+  toggleDisabled() {
+    this.disabled = !this.disabled;
+  }
+
+  toggleDisableRipple() {
+    this.disableRipple = !this.disableRipple;
+  }
 }
