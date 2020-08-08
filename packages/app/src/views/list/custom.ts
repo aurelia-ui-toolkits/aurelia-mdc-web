991bbf12d32e3231c66cc8004b569b588e1d6db5
Maxim Balaganskiy <m.balaganskiy@gmail.com>
2020-08-08 22:50:15 +1000

    feat(app): add list snippets

--- packages/app/src/views/list/custom.ts
+++ packages/app/src/views/list/custom.ts
@@ -1,17 +1,4 @@
-import customHtml from '!!raw-loader!./custom.html';
-import customSass from '!!raw-loader!./custom.scss';
-import customCode from '!!raw-loader!./custom';
-
-export class Examples {
-  customHtml = customHtml;
-  customSass = customSass;
-  customCode = customCode;
-
-  expanded: boolean;
-  toggleExpanded() {
-    this.expanded = !this.expanded;
-  }
-
+export class Custom {
   folders = [
     { name: 'Photos', icon: 'folder', addDate: 'Jan 9, 2015' },
     { name: 'Recipes', icon: 'folder', addDate: 'Jan 17, 2015' },
@@ -22,5 +9,4 @@ export class Examples {
     { name: 'Vacation Itinerary', icon: 'insert_drive_file', addDate: 'Jan 10, 2015' },
     { name: 'Kitchen Remodel', icon: 'insert_drive_file', addDate: 'Jan 20, 2015' }
   ];
-
 }
