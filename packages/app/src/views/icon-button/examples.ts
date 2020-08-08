81deb54341144d8d955e217bc18d5a7e772ecfcb
Maxim Balaganskiy <m.balaganskiy@gmail.com>
2020-08-08 16:07:12 +1000

    feat(app): add icon-button snippets

--- packages/app/src/views/icon-button/examples.ts
+++ packages/app/src/views/icon-button/examples.ts
@@ -1,1 +1,15 @@
-export class Examples { }
+import densityHtml from '!!raw-loader!./density.html';
+import densitySass from '!!raw-loader!./density.scss';
+import themeHtml from '!!raw-loader!./theme.html';
+import themeSass from '!!raw-loader!./theme.scss';
+import toggleHtml from '!!raw-loader!./toggle.html';
+import buttonHtml from '!!raw-loader!./button.html';
+
+export class Examples {
+  densityHtml = densityHtml;
+  densitySass = densitySass;
+  themeHtml = themeHtml;
+  themeSass = themeSass;
+  toggleHtml = toggleHtml;
+  buttonHtml = buttonHtml;
+}
