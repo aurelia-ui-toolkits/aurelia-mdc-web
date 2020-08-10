b8c2cd1dfcc6057643af7dcabcdc4ee159fab023
Maxim Balaganskiy <m.balaganskiy@gmail.com>
2020-08-11 09:45:58 +1000

    fix(chips): align injected element name with the rest of the packages

--- packages/chips/src/mdc-chip-primary-action.ts
+++ packages/chips/src/mdc-chip-primary-action.ts
@@ -4,13 +4,12 @@ import { customElement, inlineView, inject, bindable } from 'aurelia-framework';
 @inlineView("<template class='mdc-chip__action--primary' role='${role}' tabIndex=${tabIndex}><slot></slot></template>")
 @customElement("mdc-chip-primary-action")
 export class MdcChipPrimaryAction {
-    @bindable role: string = "button";
-    @bindable tabIndex: number = 0;
+  @bindable role: string = "button";
+  @bindable tabIndex: number = 0;
 
-    constructor(public element: HTMLElement) {
-    }
+  constructor(public root: HTMLElement) { }
 
-    focus() {
-        this.element.focus();
-    }
+  focus() {
+    this.root.focus();
+  }
 }
