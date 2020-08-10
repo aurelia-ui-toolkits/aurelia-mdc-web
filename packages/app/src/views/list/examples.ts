1aa629993ce6c85509226cc894ccb448e7f8be46
Maxim Balaganskiy <m.balaganskiy@gmail.com>
2020-08-10 11:02:55 +1000

    feat(app): add list snippets

--- packages/app/src/views/list/examples.ts
+++ packages/app/src/views/list/examples.ts
@@ -1,16 +1,30 @@
 import customHtml from '!!raw-loader!./custom.html';
 import customSass from '!!raw-loader!./custom.scss';
 import customCode from '!!raw-loader!./custom';
+import disableRippleHtml from '!!raw-loader!./disable-ripple.html';
+import shapedHtml from '!!raw-loader!./shaped.html';
+import shapedSass from '!!raw-loader!./shaped.scss';
+import singleLineHtml from '!!raw-loader!./single-line.html';
 
 export class Examples {
   customHtml = customHtml;
   customSass = customSass;
   customCode = customCode;
+  disableRippleHtml = disableRippleHtml;
+  shapedHtml = shapedHtml;
+  shapedSass = shapedSass;
+  singleLineHtml = singleLineHtml;
 
   expanded: boolean;
   toggleExpanded() {
     this.expanded = !this.expanded;
   }
+
+  items = [
+    { label: 'Wi-Fi', icon: 'network_wifi' },
+    { label: 'Bluetooth', icon: 'bluetooth' },
+    { label: 'Data Usage', icon: 'data_usage' }
+  ];
 
   folders = [
     { name: 'Photos', icon: 'folder', addDate: 'Jan 9, 2015' },
