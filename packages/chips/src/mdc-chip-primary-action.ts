df5470ca9f9af00bd7e2f22cfed9da03919499c3
Arjen de Blok <arjen@deblok.org>
2020-08-12 00:09:27 +0200

    renamed tabindex

--- packages/chips/src/mdc-chip-primary-action.ts
+++ packages/chips/src/mdc-chip-primary-action.ts
@@ -1,11 +1,11 @@
 import { customElement, inlineView, inject, bindable } from 'aurelia-framework';
 
 @inject(Element)
-@inlineView("<template class='mdc-chip__action--primary' role='${role}' tabIndex=${tabIndex}><slot></slot></template>")
+@inlineView("<template class='mdc-chip__action--primary' role='${role}' tabindex=${tabindex}><slot></slot></template>")
 @customElement("mdc-chip-primary-action")
 export class MdcChipPrimaryAction {
   @bindable role: string = "button";
-  @bindable tabIndex: number = 0;
+  @bindable tabindex: number = 0;
 
   constructor(public root: HTMLElement) { }
 
