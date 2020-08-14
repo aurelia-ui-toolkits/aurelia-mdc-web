dd2bc621a24d41f246144baa8f9397595c621757
Maxim Balaganskiy <m.balaganskiy@gmail.com>
2020-08-14 11:32:07 +1000

    feat(menu): add selection groups

---
+++ packages/menu/src/mdc-menu-selection-group.ts
@@ -0,0 +1,5 @@
+import { inlineView, customElement } from 'aurelia-framework';
+
+@inlineView('<template class="mdc-menu__selection-group"><slot></slot></template>')
+@customElement('mdc-menu-selection-group')
+export class MdcMenuSelectionGroup { }
