e37cfe9fa578ef6a21921e0c86dc2d974f18ab96
Arjen de Blok <arjen@deblok.org>
2020-08-17 16:51:00 +0200

    Added viewing source to examples

--- packages/app/src/views/chips/examples.ts
+++ packages/app/src/views/chips/examples.ts
@@ -1,4 +1,13 @@
+import defaultHtml from '!!raw-loader!./default.html';
+import choiceHtml from '!!raw-loader!./choice.html';
+import choiceLeadingHtml from '!!raw-loader!./choice-leading.html';
+import filterHtml from '!!raw-loader!./filter.html';
+import filterLeadingHtml from '!!raw-loader!./filter-leading.html';
 
 export class Examples {
-
+    defaultHtml = defaultHtml;
+    choiceHtml = choiceHtml;
+    choiceLeadingHtml = choiceLeadingHtml;
+    filterHtml = filterHtml;
+    filterLeadingHtml = filterLeadingHtml;
 }
