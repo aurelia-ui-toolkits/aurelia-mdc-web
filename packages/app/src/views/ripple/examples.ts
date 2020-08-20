2be5830c3dd8de6178928b1505b2d68d6fb725dc
Maxim Balaganskiy <m.balaganskiy@gmail.com>
2020-08-20 11:50:21 +1000

    feat(app): add ripple examples

--- packages/app/src/views/ripple/examples.ts
+++ packages/app/src/views/ripple/examples.ts
@@ -1,1 +1,9 @@
-export class Examples { }
+import basicHtml from '!!raw-loader!./basic.html';
+import primaryHtml from '!!raw-loader!./primary.html';
+import accentHtml from '!!raw-loader!./accent.html';
+
+export class Examples {
+  basicHtml = basicHtml;
+  primaryHtml = primaryHtml;
+  accentHtml = accentHtml;
+}
