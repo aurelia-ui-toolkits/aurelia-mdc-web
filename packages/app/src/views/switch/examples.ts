e656b3d06657fc014f200cc043ac46782893a18a
Maxim Balaganskiy <m.balaganskiy@gmail.com>
2020-08-25 10:15:13 +1000

    feat(app): switch examples

--- packages/app/src/views/switch/examples.ts
+++ packages/app/src/views/switch/examples.ts
@@ -1,3 +1,22 @@
+import basicHtml from '!!raw-loader!./basic.html';
+import customHtml from '!!raw-loader!./custom.html';
+import customScss from '!!raw-loader!./custom.scss';
+import themeHtml from '!!raw-loader!./theme.html';
+import themeScss from '!!raw-loader!./theme.scss';
+
 export class Examples {
+  basicHtml = basicHtml;
+  customHtml = customHtml;
+  customScss = customScss;
+  themeHtml = themeHtml;
+  themeScss = themeScss;
+
   checked: boolean = true;
+
+  changeEvent: CustomEvent;
+
+  onChange(evt: CustomEvent): void {
+    this.changeEvent = evt;
+  }
+
 }
