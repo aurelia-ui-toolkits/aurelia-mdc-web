bf8a007026e98ec93a3bc099331f58bffdcf423b
Maxim Balaganskiy <m.balaganskiy@gmail.com>
2020-08-27 11:47:13 +1000

    fix(icon): store host reference

--- packages/icon/src/mdc-icon.ts
+++ packages/icon/src/mdc-icon.ts
@@ -1,7 +1,8 @@
-import { customElement, useView, PLATFORM } from 'aurelia-framework';
+import { customElement, useView, PLATFORM, inject } from 'aurelia-framework';
 
+@inject(Element)
 @useView(PLATFORM.moduleName('./mdc-icon.html'))
 @customElement('mdc-icon')
 export class MdcIcon {
-
+  constructor(public root: HTMLElement) { }
 }
