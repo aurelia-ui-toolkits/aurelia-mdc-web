6df56a77b4af4479961ff1f8a4d99d6517e5378b
Maxim Balaganskiy <m.balaganskiy@gmail.com>
2020-08-31 11:22:42 +1000

    feat(app): add typography example snippets

--- packages/app/src/views/typography/examples.ts
+++ packages/app/src/views/typography/examples.ts
@@ -1,1 +1,13 @@
-export class Examples { }
+import bodyHtml from '!!raw-loader!./body.html';
+import captionHtml from '!!raw-loader!./caption.html';
+import headlineHtml from '!!raw-loader!./headline.html';
+import pageHtml from '!!raw-loader!./page.html';
+import subtitleHtml from '!!raw-loader!./subtitle.html';
+
+export class Examples {
+  bodyHtml = bodyHtml;
+  captionHtml = captionHtml;
+  headlineHtml = headlineHtml;
+  pageHtml = pageHtml;
+  subtitleHtml = subtitleHtml;
+}
