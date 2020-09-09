c92c9e57e53b21352f095ed15443205b7b53cd6b
Maxim Balaganskiy <m.balaganskiy@gmail.com>
2020-09-10 09:15:27 +1000

    fix(card): add disable ripple

--- packages/card/src/mdc-card-primary-action.ts
+++ packages/card/src/mdc-card-primary-action.ts
@@ -1,12 +1,14 @@
 import { customElement, inlineView } from 'aurelia-framework';
+import { bindable } from 'aurelia-typed-observable-plugin';
 
 /**
  * Optional. The main tappable area of the card. Typically contains most (or all) card content except mdc-card-actions.
  * Only applicable to cards that have a primary action that the main surface should trigger.
  * @selector mdc-card-primary-action
  */
-@inlineView('<template class="mdc-card__primary-action" mdc-ripple><slot></slot></template>')
+@inlineView('<template class="mdc-card__primary-action" mdc-ripple="disabled.bind: disableRipple"><slot></slot></template>')
 @customElement('mdc-card-primary-action')
 export class MdcCardPrimaryAction {
-
+  @bindable.booleanAttr
+  disableRipple: boolean;
 }
