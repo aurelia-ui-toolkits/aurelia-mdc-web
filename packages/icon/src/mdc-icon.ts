1efae3fe04f2245ea1bcd456612720905899770f
Maxim Balaganskiy <m.balaganskiy@gmail.com>
2020-09-16 11:33:54 +1000

    doc(icon)

--- packages/icon/src/mdc-icon.ts
+++ packages/icon/src/mdc-icon.ts
@@ -1,5 +1,8 @@
 import { customElement, useView, PLATFORM, inject } from 'aurelia-framework';
 
+/**
+ * @selector mdc-icon
+ */
 @inject(Element)
 @useView(PLATFORM.moduleName('./mdc-icon.html'))
 @customElement('mdc-icon')
