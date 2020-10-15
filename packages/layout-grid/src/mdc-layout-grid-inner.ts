f7ddb44aac2c99f0c75b216566603822d127671f
Maxim Balaganskiy <m.balaganskiy@gmail.com>
2020-10-15 15:12:11 +1100

    fix(layout-grid): class name

--- packages/layout-grid/src/mdc-layout-grid-inner.ts
+++ packages/layout-grid/src/mdc-layout-grid-inner.ts
@@ -6,4 +6,4 @@ import { customElement, inlineView } from 'aurelia-framework';
  */
 @inlineView('<template class="mdc-layout-grid__inner"><slot></slot></template>')
 @customElement('mdc-layout-grid-inner')
-export class MdcLayoutGrid { }
+export class MdcLayoutGridInner { }
