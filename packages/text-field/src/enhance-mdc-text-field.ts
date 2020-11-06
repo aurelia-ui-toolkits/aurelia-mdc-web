18ec9a8166aeb3bad2c141664826b7b9e0d757ac
Maxim Balaganskiy <m.balaganskiy@gmail.com>
2020-11-06 11:11:56 +1100

    fix(text-field): automatically replace `mdc-text-field` tag with `label`
    fixes #22

--- packages/text-field/src/enhance-mdc-text-field.ts
+++ packages/text-field/src/enhance-mdc-text-field.ts
@@ -2,10 +2,24 @@ import { viewEngineHooks } from 'aurelia-framework';
 
 @viewEngineHooks
 export class EnhanceMdcTextfield {
-	beforeCompile(template: DocumentFragment) {
-		const actions = template.querySelectorAll('[mdc-text-field]');
-		for (const i of Array.from(actions)) {
-			i.setAttribute('as-element', 'mdc-text-field');
-		}
-	}
+  beforeCompile(template: DocumentFragment) {
+    const textFields = template.querySelectorAll<HTMLElement>('mdc-text-field');
+    for (const tf of Array.from(textFields)) {
+      const label = document.createElement('label');
+
+      // Grab all of the original's attributes, and pass them to the replacement
+      for (let i = 0, l = tf.attributes.length; i < l; ++i) {
+        const nodeName = tf.attributes.item(i)!.nodeName;
+        const nodeValue = tf.attributes.item(i)!.nodeValue;
+        label.setAttribute(nodeName, nodeValue!);
+      }
+      label.setAttribute('as-element', 'mdc-text-field');
+
+      // Persist contents
+      label.innerHTML = tf.innerHTML;
+
+      // Switch!
+      tf.parentNode!.replaceChild(label, tf);
+    }
+  }
 }
