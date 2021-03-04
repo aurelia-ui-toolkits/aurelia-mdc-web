3fa58341f5f7d003e7863ea7185572638e24296f
Maxim Balaganskiy <m.balaganskiy@gmail.com>
2021-03-04 11:23:01 +1100

    Upgrade aurelia

--- packages/validation/src/index.ts
+++ packages/validation/src/index.ts
@@ -1,2 +1,2 @@
 export { MdcValidationResultPresenter } from './mdc-validation-result-presenter';
-export { MdcValidationControllerFactory } from './mdc-validatio-controller-factory';
+export { MdcValidationControllerFactory } from './mdc-validation-controller-factory';
