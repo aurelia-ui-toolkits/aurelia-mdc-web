9666d7559f37a427baad1358d7355420acc8f349
Maxim Balaganskiy <m.balaganskiy@gmail.com>
2021-03-05 13:02:02 +1100

    fix(dialog): patch focus trap
    This allows for putting the initial focus on an mdc-select

--- packages/dialog/src/index.ts
+++ packages/dialog/src/index.ts
@@ -1,4 +1,5 @@
 import { FrameworkConfiguration, PLATFORM } from 'aurelia-framework';
+import { FocusTrap } from '@material/dom/focus-trap';
 
 export { MdcDialog } from './mdc-dialog';
 export { MdcDialogService, IMdcDialogOptions } from './mdc-dialog-service';
@@ -11,3 +12,28 @@ export function configure(config: FrameworkConfiguration) {
     PLATFORM.moduleName('./mdc-dialog-content')
   ]);
 }
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+(FocusTrap.prototype as any).getFocusableElements = function (root: HTMLElement): HTMLElement[] {
+  const focusableEls =
+    [].slice.call(root.querySelectorAll(
+      '[autofocus], [tabindex], a, input, textarea, select, button, .mdc-select__anchor')) as
+    HTMLElement[];
+  return focusableEls.filter((el) => {
+    const isDisabledOrHidden = el.getAttribute('aria-disabled') === 'true' ||
+      el.getAttribute('disabled') !== null ||
+      el.getAttribute('hidden') !== null ||
+      el.getAttribute('aria-hidden') === 'true';
+    const isTabbableAndVisible = (el.tabIndex >= 0 || el.classList.contains('mdc-select__anchor')) &&
+      el.getBoundingClientRect().width > 0 &&
+      !el.classList.contains('mdc-dom-focus-sentinel') && !isDisabledOrHidden;
+
+    let isProgrammaticallyHidden = false;
+    if (isTabbableAndVisible) {
+      const style = getComputedStyle(el);
+      isProgrammaticallyHidden =
+        style.display === 'none' || style.visibility === 'hidden';
+    }
+    return isTabbableAndVisible && !isProgrammaticallyHidden;
+  });
+};
