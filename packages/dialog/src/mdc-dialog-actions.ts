392796116118f5334b4bcf0bfd65c19325e655a3
Maxim Balaganskiy <m.balaganskiy@gmail.com>
2021-03-06 12:21:48 +1100

    fix(dialog): properly monitor for child buttons

--- packages/dialog/src/mdc-dialog-actions.ts
+++ packages/dialog/src/mdc-dialog-actions.ts
@@ -1,6 +1,7 @@
-import { customElement, inject } from 'aurelia';
+import { customElement, inject, children } from 'aurelia';
 import { processContent } from '@aurelia/runtime-html';
 import { defaultSlotProcessContent } from '@aurelia-mdc-web/base';
+import { MdcButton } from '@aurelia-mdc-web/button';
 
 /**
  * Optional. Footer area containing the dialog's action buttons.
@@ -10,15 +11,17 @@ import { defaultSlotProcessContent } from '@aurelia-mdc-web/base';
 @customElement({ name: 'mdc-dialog-actions', template: '<template class="mdc-dialog__actions"><au-slot></au-slot></template>' })
 @processContent(defaultSlotProcessContent)
 export class MdcDialogActions {
-  constructor(private root: HTMLElement) { }
+  @children({ query: controller => controller.host.querySelectorAll('.mdc-button') })
+  buttons: MdcButton[];
+  buttonsChanged() {
+    for (let i = 0; i < this.buttons.length; ++i) {
+      this.buttons[i].root.classList.add('mdc-dialog__button');
+    }
+  }
 
   attached() {
-    const children = this.root.children;
-    for (let i = 0; i < children.length; ++i) {
-      const child = children[i];
-      if (child.classList.contains('mdc-button')) {
-        child.classList.add('mdc-dialog__button');
-      }
+    if (this.buttons) {
+      this.buttonsChanged();
     }
   }
 }
