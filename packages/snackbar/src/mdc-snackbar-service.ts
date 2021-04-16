50aafa1ac7cd53e243a37cf00b381195194e18a6
Maxim Balaganskiy <m.balaganskiy@gmail.com>
2021-04-16 12:30:48 +1000

    fix(snackbar): DOM structure

--- packages/snackbar/src/mdc-snackbar-service.ts
+++ packages/snackbar/src/mdc-snackbar-service.ts
@@ -22,7 +22,8 @@ export class MdcSnackbarService {
   constructor(private templatingEngine: TemplatingEngine) { }
 
   async open(label: string, actions?: string | string[], options?: Partial<ISnackbarOptions>) {
-    const snackbar = document.createElement('mdc-snackbar') as IMdcSnackbarElement;
+    const snackbar = document.createElement('aside') as IMdcSnackbarElement;
+    snackbar.setAttribute('as-element', 'mdc-snackbar');
     snackbar.setAttribute(`${strings.CLOSED_EVENT}.trigger`, 'handleClosed($event)');
     document.body.appendChild(snackbar);
     let closedResolver: (reason?: string | PromiseLike<string> | undefined) => void;
@@ -47,27 +48,6 @@ export class MdcSnackbarService {
     if (options) {
       Object.assign(vm, options);
     }
-    // if (options?.timeout !== undefined) {
-    //   vm.timeout = options.timeout;
-    // }
-    // if (options?.closeOnEscape !== undefined) {
-    //   vm.closeOnEscape = options.closeOnEscape;
-    // }
-    // if (options?.dismissible !== undefined) {
-    //   vm.dismissible = options.dismissible;
-    // }
-    // if (options?.stacked !== undefined) {
-    //   vm.stacked = options.stacked;
-    // }
-    // if (options?.classes !== undefined) {
-    //   vm.classes = options.classes;
-    // }
-    // if (options?.actionClasses !== undefined) {
-    //   vm.actionClasses = options.actionClasses;
-    // }
-    // if (options?.stacked !== undefined) {
-    //   vm.stacked = options.stacked;
-    // }
     await vm.initialised;
     snackbar.au.controller.viewModel.open();
     return closedPromise;
