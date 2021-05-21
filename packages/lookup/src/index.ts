f372d8bd36528e65abd82337c201cc203d303180
Maxim Balaganskiy <m.balaganskiy@gmail.com>
2021-05-21 10:50:16 +1000

    fix(lookup): protect against missing virtualize plugin

--- packages/lookup/src/index.ts
+++ packages/lookup/src/index.ts
@@ -1,9 +1,19 @@
-import { FrameworkConfiguration, PLATFORM } from 'aurelia-framework';
+import { FrameworkConfiguration, PLATFORM, ViewResources } from 'aurelia-framework';
 import { MdcDefaultLookupConfiguration } from './mdc-lookup-configuration';
 
 export { MdcLookup, IMdcLookupElement } from './mdc-lookup';
 
 export function configure(frameworkConfig: FrameworkConfiguration, callback?: (config: MdcDefaultLookupConfiguration) => void) {
+  // make sure missing plugin does not crash the template compiler
+  const resources = frameworkConfig.container.get(ViewResources);
+  resources.getAttribute = (fn => function (attrName: string) {
+    if (attrName === 'virtual-repeat' && !this.attributes['virtual-repeat']) {
+      attrName = 'repeat';
+    }
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+    return fn.call(this, attrName);
+  })(resources.getAttribute);
+
   frameworkConfig.globalResources([
     PLATFORM.moduleName('./mdc-lookup')
   ]);
