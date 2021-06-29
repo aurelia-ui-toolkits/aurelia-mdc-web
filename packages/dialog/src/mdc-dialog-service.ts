9f08b4c1ac5a471c63231002fed684dda01c7283
Maxim Balaganskiy <m.balaganskiy@gmail.com>
2021-06-29 16:50:09 +1000

    fix(dialog): re-layout ripple in new dialog service

--- packages/dialog/src/mdc-dialog-service.ts
+++ packages/dialog/src/mdc-dialog-service.ts
@@ -1,9 +1,10 @@
 import { MdcDialog } from './mdc-dialog';
-import { inject, IPlatform, IContainer, IAppRoot, LifecycleFlags, createElement } from 'aurelia';
+import { inject, IPlatform, IContainer, IAppRoot, LifecycleFlags, createElement, CustomAttribute } from 'aurelia';
 import { MDCDialogCloseEvent } from '@material/dialog';
 import { Scope } from '@aurelia/runtime';
 import { CustomElement } from '@aurelia/runtime-html';
 import { Constructable } from '@aurelia/kernel';
+import { IMdcRippleElement, MdcRipple } from '@aurelia-mdc-web/ripple';
 
 /** Dialog service open method options */
 export interface IMdcDialogOptions {
@@ -15,7 +16,7 @@ export interface IMdcDialogOptions {
 }
 
 interface IMdcDialogBindingContext {
-  handleClosing(evt: MDCDialogCloseEvent): void;
+  handleClosed(evt: MDCDialogCloseEvent): void;
   handleOpened(): void;
 }
 
@@ -26,15 +27,15 @@ export class MdcDialogService {
 
   /** Opens the dialog specified in the options */
   async open(options: IMdcDialogOptions) {
-    let closingResolver: (action?: string | PromiseLike<string> | undefined) => void;
-    const closingPromise = new Promise<string>(r => closingResolver = r);
+    let closedResolver: (action?: string | PromiseLike<string> | undefined) => void;
+    const closedPromise = new Promise<string>(r => closedResolver = r);
     let openedResolver: (value?: unknown) => void;
     const openedPromise = new Promise(r => openedResolver = r);
     const bindingContext: IMdcDialogBindingContext = {
-      handleClosing: (evt: MDCDialogCloseEvent) => {
-        closingResolver(evt.detail.action);
+      handleClosed: (evt: MDCDialogCloseEvent) => {
+        closedResolver(evt.detail.action);
         sv.deactivate(sv, this.appRoot.controller, LifecycleFlags.none);
-        dialogVm.root.removeEventListener('mdcdialog:closing', bindingContext.handleClosing);
+        dialogVm.root.removeEventListener('mdcdialog:closed', bindingContext.handleClosed);
         dialogVm.root.removeEventListener('mdcdialog:opened', bindingContext.handleOpened);
         dialogEl.remove();
       },
@@ -51,7 +52,7 @@ export class MdcDialogService {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const vm = sv.children![0].viewModel as any;
     const dialogVm = CustomElement.for<MdcDialog>(dialogEl.querySelector('mdc-dialog')!).viewModel;
-    dialogVm.root.addEventListener('mdcdialog:closing', bindingContext.handleClosing);
+    dialogVm.root.addEventListener('mdcdialog:closed', bindingContext.handleClosed);
     dialogVm.root.addEventListener('mdcdialog:opened', bindingContext.handleOpened);
     if (vm.activate) {
       const activateResult = vm.activate(options.model);
@@ -63,9 +64,15 @@ export class MdcDialogService {
     dialogVm.open();
 
     await openedPromise;
-
+    // re-layout ripple elements because dialogs use `transform: scale(.8)` and initial layout is incorrect
+    const ripples = Array.from(dialogVm.root.querySelectorAll<IMdcRippleElement>('.mdc-ripple-upgraded'));
+    await Promise.all(ripples.map(async x => {
+      const ripple = CustomAttribute.for<MdcRipple>(x, 'mdc-ripple');
+      await ripple?.viewModel.initialised;
+      ripple?.viewModel.foundation?.layout();
+    }));
     dialogVm.createFocusTrap();
     this.platform.taskQueue.queueTask(() => dialogVm.focusTrap?.trapFocus());
-    return closingPromise;
+    return closedPromise;
   }
 }
