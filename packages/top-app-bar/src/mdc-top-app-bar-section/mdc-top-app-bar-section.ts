ad021df1d16df0faf1de2347760b053ef4dd9a5a
Maxim Balaganskiy <m.balaganskiy@gmail.com>
2021-06-29 16:15:56 +1000

    fix(top-app-bar): rename align attribute

--- packages/top-app-bar/src/mdc-top-app-bar-section/mdc-top-app-bar-section.ts
+++ packages/top-app-bar/src/mdc-top-app-bar-section/mdc-top-app-bar-section.ts
@@ -1,7 +1,8 @@
+import { booleanAttr } from '@aurelia-mdc-web/base';
 import { customElement, bindable } from 'aurelia';
 
 @customElement('mdc-top-app-bar-section')
 export class MdcTopAppBarSection {
-  @bindable
-  align: 'start' | 'end' = 'start';
+  @bindable({ set: booleanAttr })
+  end: boolean;
 }
