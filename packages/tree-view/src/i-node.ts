ca5951647b652eda39049848e01f3078e2da3128
Maxim Balaganskiy <m.balaganskiy@gmail.com>
2021-07-06 17:10:15 +1000

    fix(tree-view): refactor child tree view promise assignment

--- packages/tree-view/src/i-node.ts
+++ packages/tree-view/src/i-node.ts
@@ -1,5 +1,8 @@
+import { MdcTreeView } from './mdc-tree-view';
+
 export interface INode {
   children?: INode[];
   expanded?: boolean;
   selected?: boolean;
+  childTreeViewPromise?: Promise<MdcTreeView>;
 }
