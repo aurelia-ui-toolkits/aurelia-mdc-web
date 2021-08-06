3cb9995acd70dc8bb212fa099843e7ed8225602a
Maxim Balaganskiy <m.balaganskiy@gmail.com>
2021-08-06 16:44:37 +1000

    clean(app)

--- packages/app/src/views/ripple/ripple-examples.ts
+++ packages/app/src/views/ripple/ripple-examples.ts
@@ -1,17 +1,8 @@
 import basicHtml from '!!raw-loader!./basic/basic.html';
-import primaryHtml from '!!raw-loader!./primary/primary.html';
-import accentHtml from '!!raw-loader!./accent/accent.html';
 
 import { Basic } from './basic/basic';
-import { Primary } from './primary/primary';
-import { Accent } from './accent/accent';
 
 export class RippleExamples {
   basicHtml = basicHtml;
-  primaryHtml = primaryHtml;
-  accentHtml = accentHtml;
-
   basic = Basic;
-  primary = Primary;
-  accent = Accent;
 }
