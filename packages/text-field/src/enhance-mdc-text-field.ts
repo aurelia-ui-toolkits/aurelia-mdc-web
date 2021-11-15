c9ca02d440a25e6813d5b99a82baaa1dec3b23ed
Maxim Balaganskiy <m.balaganskiy@gmail.com>
2021-11-15 22:13:37 +1100

    clean(text-field)

--- packages/text-field/src/enhance-mdc-text-field.ts
+++ packages/text-field/src/enhance-mdc-text-field.ts
@@ -3,8 +3,7 @@ import { templateCompilerHooks } from 'aurelia';
 @templateCompilerHooks
 export class EnhanceMdcTextfield {
   compiling(template: HTMLElement | HTMLTemplateElement) {
-    // template.innerHTML = template.innerHTML.replaceAll('<mdc-text-field>','<label as-element="mdc-text-field">');
-    if (template.innerHTML.includes('standart text field')) {
+    if (template.innerHTML.includes('<mdc-text-field')) {
       template.innerHTML = template.innerHTML
         .replaceAll('<mdc-text-field ', '<label as-element="mdc-text-field" ')
         .replaceAll('</mdc-text-field>', '</label>');
