7e585b780d0744a36c086dce053c3f84807aed0e
Maxim Balaganskiy <m.balaganskiy@gmail.com>
2022-05-04 15:52:07 +1000

    feat(base): add helper function

--- packages/base/src/index.ts
+++ packages/base/src/index.ts
@@ -8,6 +8,7 @@ export { MATERIAL_PALETTE } from './material-palette';
 export { boolean, booleanAttr, date, number, string } from './interceptors';
 export { defaultSlotProcessContent } from './default-slot-process-content';
 export { MdcFocusTrap } from './attributes/mdc-focus-trap';
+export { nextElement } from './next-element';
 
 export const BaseConfiguration = {
   register(container: IContainer): IContainer {
