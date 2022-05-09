543ab1ddd02917333738204860b9c95ce70bf614
Maxim Balaganskiy <m.balaganskiy@gmail.com>
2022-05-09 10:15:54 +1000

    docs(dialog): add sheet example

--- packages/app/src/views/dialog/examples.ts
+++ packages/app/src/views/dialog/examples.ts
@@ -4,6 +4,7 @@ import { ServiceDialog } from './service-dialog';
 import inlineHtml from '!!raw-loader!./inline.html';
 import serviceHtml from '!!raw-loader!./service-dialog.html';
 import serviceCode from './service-dialog.ts.raw';
+import sheetHtml from '!!raw-loader!./sheet.html';
 
 @autoinject
 export class Examples {
@@ -12,6 +13,7 @@ export class Examples {
   inlineHtml = inlineHtml;
   serviceHtml = serviceHtml;
   serviceCode = serviceCode;
+  sheetHtml = sheetHtml;
 
   async open() {
     alert(await this.dialogService.open({ viewModel: ServiceDialog, model: { caption: 'Select an account' } }));
